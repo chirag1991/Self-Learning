50deaba4aad6d078b1c8c09e5f56a2996d139fd3
Chirag Valand <chiragvaland22@gmail.com>
2022-10-25 13:03:03 -0600

    multiple state handling

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -1,23 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = ()=>
 {
+    const [enterdTitle, setEnteredTitle] = useState('');
+    const [enterdAmount, setEnteredAmount] = useState('');
+    const [enterdDate, setEnteredDate] = useState('');
   
+    const titleChangeHandler = (event) =>
+    {
+        setEnteredTitle(event.target.value);
+    };
+    const amountChangeHandler = (event) =>
+    {
+        setEnteredAmount(event.target.value);
+    };
+    const dateChangeHandler = (event) =>
+    {
+        setEnteredDate(event.target.value);
+    };
+
+    // const formSubmitHandler= (event)=>
+    // {
+    //     eve
+    // }
+
     return(
-        <form>
+        <form onSubmit={formSubmitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text"/>
+                    <input type="text" onChange={titleChangeHandler}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number"   min="0.01" step="0.01"   />
+                    <input type="number"  onChange={amountChangeHandler} min="0.01" step="0.01"   />
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" />
+                    <input type="date" onChange={dateChangeHandler}/>
                 </div>
             </div>
             <div className="new-expense__actions">
