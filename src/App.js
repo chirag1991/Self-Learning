67c12d10254ce7b2aa56127824522808d6ad11b4
Chirag Valand <chiragvaland22@gmail.com>
2022-10-25 10:01:15 -0600

    ExpenseItem Component created with  CSS styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import ExpenseItem from "./Components/ExpenseItem";
 
 function App(){
     return(
         <div>
         <h1>First Componrnt from App.js</h1>
-        <h1>added some code after creating hot fix branch</h1>
+        <ExpenseItem></ExpenseItem>
         </div>
     );
 }
