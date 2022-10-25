6be12436947d16c51dacf3db9ac6a4a8b741e5f1
Chirag Valand <chiragvaland22@gmail.com>
2022-10-25 09:19:37 -0600

    changed some code in hotfix branch

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from "react";
 
 function App(){
     return(
+        <div>
         <h1>First Componrnt from App.js</h1>
+        <h1>added some code after creating hot fix branch</h1>
+        </div>
     );
 }
 export default App;
